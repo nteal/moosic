0171a6fc676a87447434204c526eeec140bce171
Nicholas Teal <nteal15@gmail.com>
2018-02-06 16:02:11 -0600

    clickable link for happy

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const queryString = require('query-string');
 var mood = require('../database-mongo/index');
 var moodHelper = require('../database-mongo/moodHelper');
-const config = require('../config');
+// const config = require('../config');
 
 var app = express();
 app.use(bodyParser.json())
@@ -46,47 +46,69 @@ app.post('/moods', function(req, res){
   //req.body.query is mood string]
     //need to convert it to correct code
 
-  const testData = config.fakeData.RESPONSE[0].ALBUM[0];
-  const testArtist = testData.ARTIST[0].VALUE;
-  const testTitle = testData.TITLE[0].VALUE;
-  console.log(testTitle + testArtist);
-  axios.get(`https://www.googleapis.com/youtube/v3/search?q=cute dogs&key=${googleKey}&part=snippet`)
-  .then((youtubeData) => {
-    console.log(youtubeData);
-    const videoId = youtubeData.data.items[0].id.videoId;
-    console.log('your new url is: ', videoId);
-    res.header(200).send(videoId);
-  })
-  .catch((err) => {console.log('you got an err in your get vids: ', err)});
-  // res.header(200).send(testTitle + testArtist);
+  // const testData = config.fakeData.RESPONSE[0].ALBUM[0];
+  // const testArtist = testData.ARTIST[0].VALUE;
+  // const testTitle = testData.TITLE[0].VALUE;
+  // console.log(testTitle + testArtist);
+  // axios.get(`https://www.googleapis.com/youtube/v3/search?q=cute dogs&key=${googleKey}&part=snippet`)
+  // .then((youtubeData) => {
+  //   console.log(youtubeData);
+  //   const videoId = youtubeData.data.items[0].id.videoId;
+  //   console.log('your new url is: ', videoId);
+  //   res.header(200).send(videoId);
+  // })
+  // .catch((err) => {console.log('you got an err in your get vids: ', err)});
 
-  // axios.get(`https://c1339077868.web.cddbp.net/webapi/json/1.0/radio/create?genre=36065&genre=36054&client=${clientId}&user=${userId}`,
-  // {
-  // }).then((response) => {
-  //   console.log('your response is: ', response);
-  //   const musicResponse = response.data;
-  //   const radioId = musicResponse.RESPONSE[0].RADIO[0].ID;
-  //   axios.get(`https://c1339077868.web.cddbp.net/webapi/json/1.0/radio/setting?radio_id=${radioId}&filter_mood=42949&client=${clientId}&user=${userId}`)
-  //   .then((moodStation) => {
-  //     const moodMusicResponse = moodStation.data;
-  //     console.log('the body in your mood request is: ', moodMusicResponse);
-  //     const moodMusicAlbum = moodMusicResponse.RESPONSE[0].ALBUM;
+
+
+
+
+
+  axios.get(`https://c1339077868.web.cddbp.net/webapi/json/1.0/radio/create?genre=36065&genre=36054&client=${clientId}&user=${userId}`,
+  {
+  }).then((response) => {
+    console.log('your response is: ', response);
+    const musicResponse = response.data;
+    const radioId = musicResponse.RESPONSE[0].RADIO[0].ID;
+    axios.get(`https://c1339077868.web.cddbp.net/webapi/json/1.0/radio/setting?radio_id=${radioId}&filter_mood=42949&client=${clientId}&user=${userId}`)
+    .then((moodStation) => {
+      const moodMusicResponse = moodStation.data;
+      console.log('the body in your mood request is: ', moodMusicResponse);
+      const moodMusicAlbum = moodMusicResponse.RESPONSE[0].ALBUM[0];
       
-  //     //PUT YOUR YOUTUBE CALL HERE
-  //     moodHelper.createNew(req.body.query)
-  //     .then((newMood) => {
-  //       res.status(201).send(moodMusicAlbum);
-  //       res.end();
-  //     })
-  //     .catch((err) => {
-  //       console.log('yah hit an err trying to post to /moods: ', err);
-  //     });
+      //PUT YOUR YOUTUBE CALL HERE
 
-  //   })
-  //   .catch((err) => {console.log('ya hit an error trying to get the mood station', err)});
+      // const testData = config.fakeData.RESPONSE[0].ALBUM[0];
+      const moodArtist = moodMusicAlbum.ARTIST[0].VALUE;
+      const moodTitle = moodMusicAlbum.TITLE[0].VALUE;
+      const moodSearch = moodTitle + ' ' + moodArtist
+      
+      console.log('mood search is: ', moodSearch);
 
-  //   })
-  //   .catch((err) => {console.log('error in axios', err)});
+      axios.get(`https://www.googleapis.com/youtube/v3/search?q=happy&key=${googleKey}&part=snippet`)
+      // axios.get(`https://www.googleapis.com/youtube/v3/search?q=${moodSearch}&key=${googleKey}&part=snippet`)
+      .then((youtubeData) => {
+        console.log('yourube data is: ', youtubeData);
+        const videoId = youtubeData.data.items[0].id.videoId;
+        console.log('your new url is: ', videoId);
+        // res.header(200).send(videoId);
+        moodHelper.createNew(req.body.query)
+        .then((newMood) => {
+        res.status(201).send(videoId);
+        res.end();
+      })
+      .catch((err) => {
+        console.log('yah hit an err trying to post to /moods: ', err);
+      })
+      .catch((err) => {console.log('you got an err in your get vids: ', err)});
+      
+      });
+
+    })
+    .catch((err) => {console.log('ya hit an error trying to get the mood station', err)});
+
+    })
+    .catch((err) => {console.log('error in axios', err)});
 
   
 });
